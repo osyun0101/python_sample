f5cfa41c08cc90ffd9b13acc1c29a8bb0c064222
natyudon <61405828+natyudon@users.noreply.github.com>
2021-01-12 00:06:02 +0900

    動画から魚が写っている領域描画完了

--- fishvideo_find.py
+++ fishvideo_find.py
@@ -1,11 +1,18 @@
 import cv2,os,pickle,copy
 
+with open('fish.pkl', "rb") as fp:
+  clf = pickle.load(fp)
 img_last = None
 cap = cv2.VideoCapture('fish.mp4')
+path = './bestshot'
+count = 0
+frame_count = 0
+if not os.path.isdir(path): os.mkdir(path)
 
 while True:
   is_ok, frame = cap.read()
   if not is_ok: break
+  frame_count += 1
   frame = cv2.resize(frame, (640, 360))
   frame2 = copy.copy(frame)
 
@@ -13,10 +20,29 @@ while True:
   img = cv2.GaussianBlur(img, (7, 7), 0)
   im = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)[1]
 
-  if not img_last is None: continue
-  frame_diff = cv2.absdiff(img_last, im)
-  cnts = cv2.findContours(frame_diff, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[1]
+  if not img_last is None: 
+    frame_diff = cv2.absdiff(img_last, im)
+    cnts = cv2.findContours(frame_diff, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[1]
 
-  for pt in cnts:
-    fre = cv2.boudingRect(pt)
-    
+    fish_count = 0
+    for pt in cnts:
+      x,y,w,h = cv2.boundingRect(pt)
+      if w < 100 or w > 500: continue
+      fre = frame[y:y+h, x:x+w]
+      fre = cv2.resize(fre, (64, 32))
+      imx = fre.reshape(-1, )
+      pre_y = clf.predict([imx])
+      if pre_y[0] == 1:
+        fish_count += 1
+        cv2.rectangle(frame2, (x,y),(x+w,y+h),(0,255,0),3)
+    if fish_count > 3:
+      name = path + '/' + str(count) + ".jpg"
+      cv2.imwrite(name, frame)
+      count += 1
+
+  cv2.imshow('fishvideo', frame2)
+  if cv2.waitKey(1) == 13: break
+  img_last = im
+cap.release()
+cv2.destroyAllWindows()
+print('ok', count, "/", frame_count)
